d82bc32acebbf2e060bcd86fd7422c4005f0f490
Gautom Das <gautomdas4@gmail.com>
2018-05-17 04:32:21 -0400

    Added A Lot

--- animation/sketch.js
+++ animation/sketch.js
@@ -1,11 +1,28 @@
-let backG;
+let kitten;
+let add;
+
+function preload() {
+  kitten = loadImage('../animation/kittens/kitten.jpg');
+}
 
 function setup() {
-  createCanvas(windowWidth, 1000, WEBGL);
+  //createCanvas(1000, 1000, WEBGL);
+  add = 0;
+  createCanvas(window.innerWidth, 800, WEBGL);
 }
 
 function draw() {
   background(200);
-  rotateY(frameCount * 0.001);
-  cylinder(500, 200);
+  console.log(frameCount);
+  if(mouseX<((window.innerWidth)/2)){
+    add = add+0.01;
+    rotateY(add);
+  }
+  else{
+    add = add-0.01;
+    rotateY(add);
+  }
+  console.log(add);
+  cylinder(500, 100);
+  texture(kitten);
 }
