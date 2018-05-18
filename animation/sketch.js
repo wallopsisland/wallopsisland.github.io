e94455ed43ad3f6e18657beddcbba0d580aa6d98
Gautom Das <gautomdas4@gmail.com>
2018-05-18 02:04:19 -0400

    General

--- animation/sketch.js
+++ animation/sketch.js
@@ -1,29 +1,32 @@
 let kitten;
 let add;
 
+
 function preload() {
   kitten = loadImage('../animation/Design.jpeg');
 }
 
 function setup() {
   //createCanvas(1000, 1000, WEBGL);
+  var cnv = createCanvas(800, 800, WEBGL);
+  var x = 100 ;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
   add = 0;
-  createCanvas(window.innerWidth, 800, WEBGL);
   scale(0.25);
 }
 
 function draw() {
   background(200);
   console.log(frameCount);
-  if(mouseX<((window.innerWidth)/2)){
-    add = add+0.01;
-    rotateY(add);
-  }
-  else{
-    add = add-0.01;
-    rotateY(add);
-  }
-  console.log(add);
-  cylinder(1303/3.4, 1024/3.4);
+  var slider = document.getElementById("myRange");
+
+  document.getElementById("Position").innerHTML = add.toString();
+  document.getElementById("Section").innerHTML = slider.value.toString();
+  document.getElementById("Fact").innerHTML = "eEEp";
+
+  rotateY(slider.value/45);
+
+  cylinder(1303/3, 1024/3);
   texture(kitten);
 }
